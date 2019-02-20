cb30492f40e07e799d2a3f077e8246a98337ce62
ozaki <fs65ugumori@yahoo.co.jp>
2019-02-20 19:25:06 +0900

    get ec2 instances

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const AWS = require('aws-sdk');
 const app = express();
+
+const ec2 = new AWS.EC2();
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -13,6 +16,19 @@ app.use(function(req, res, next) {
 
 app.get('/', (req, res) => {
   res.send('Hello');
+});
+
+app.get('/instances', (req, res) => {
+  const params = {};
+  ec2.describeInstances(params, function(err, data) {
+    if (err) {
+      console.log('Error', err.stack);
+      res.send(err.stack);
+    } else {
+      console.log('Success', JSON.stringify(data));
+      res.send(JSON.stringify(data));
+    }
+  });
 });
 
 const port = '8080';
